ed3bbae6450a9664884df8b3f3b5d6930f7bf910
Jerome Etienne <jerome.etienne@gmail.com>
2011-03-04 15:24:43 +0100

    more on acewidget (when done, port it to its own repo)

---
+++ www/acewidget/README.md
@@ -0,0 +1,23 @@
+# ace widget
+
+AceWidget is a widget iframe which make including ace in your page simple.
+
+   <iframe src="http://acewidget.org"></iframe>
+   
+   http://acewidget.org/?theme=twilight&mode=javascript
+   
+   http://acewidget.org/
+   
+## API
+
+The api is done via the usual
+[window.postMessage()](https://developer.mozilla.org/en/DOM/window.postMessage).
+
+## vision
+
+* be super simple to install (no server)
+  * so a iframe with an api
+* provide all API from https://gist.github.com/raw/840439/be9ff7a81aeb9e35925efb320f4a7813d86d9342/issue%20in%20encoding
+* that's it
+
+
---
+++ www/acewidget/main.js
@@ -0,0 +1,40 @@
+/**
+ * * docs https://github.com/ajaxorg/ace/wiki/Embedding---API
+*/
+
+var AceEditor	= function(){
+	this.editor	= ace.edit("editor");		
+	var theme	= jQuery.url.param("theme")	|| null;
+	var mode	= jQuery.url.param("mode")	|| null;
+	if( theme )	this.editor.setTheme("ace/theme/"+theme);
+	if( mode )	this.editor.getSession().setMode(new (require("ace/mode/"+mode).Mode)());		
+
+
+	window.addEventListener("message", function(event){
+		// keep message IIF in a iframe
+		if( window.parent === window )		return;
+		// keep only message from the parent window
+		if( event.source !== window.parent)	return;
+		// parse the event
+		var eventFull	= JSON.parse(event.data);
+		var eventType	= eventFull.type;
+		var eventData	= eventFull.data;
+		//console.log("eventFull", eventFull);
+		//console.log("window message", event.data, event.origin);
+		var methodName	= "on" + eventType.substr(0,1).toUpperCase() + eventType.substr(1);
+		if( methodName in this ){
+			this[methodName](eventData);
+		}
+	}.bind(this), false);	
+}
+
+AceEditor.prototype.onLoadText	= function(data){
+	var text	= data.text;
+	this.editor.getSession().getDocument().setValue(text);
+	//this.editor.getSession().setTabSize(8);
+}
+
+var main	= function(){
+//	jQuery("#editor").html("console.log('dd');");
+	var editor	= new AceEditor();
+}
